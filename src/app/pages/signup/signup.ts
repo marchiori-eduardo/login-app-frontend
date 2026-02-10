3ecdfcf66bf36cdf7d32131534d3b65d6cdf79b3
Eduardo Marchiori <marchiorimarchiori2@gmail.com>
2026-02-10 12:32:16 -0300

    feat: create signup page

--- src/app/pages/signup/signup.ts
+++ src/app/pages/signup/signup.ts
@@ -6,13 +6,15 @@ import { Router } from '@angular/router';
 import { LoginService } from '../../services/login';
 import { ToastrService } from 'ngx-toastr';
 
-interface LoginForm {
+interface SignupForm {
+  name: FormControl,
   email: FormControl,
-  password: FormControl
+  password: FormControl,
+  confirmPassword: FormControl
 }
 
 @Component({
-  selector: 'app-login',
+  selector: 'app-signup',
   imports: [
     DefaultLoginLayout,
     ReactiveFormsModule,
@@ -21,32 +23,34 @@ interface LoginForm {
   providers: [
     LoginService
   ],
-  templateUrl: './login.html',
-  styleUrl: './login.scss',
+  templateUrl: './signup.html',
+  styleUrl: './signup.scss',
 })
-export class Login { 
-  loginForm!: FormGroup<LoginForm>;
+export class Signup { 
+  signupForm!: FormGroup<SignupForm>;
 
   constructor(
     private router: Router, 
     private loginService: LoginService,
     private toastr: ToastrService
   ) {
-    this.loginForm = new FormGroup({
+    this.signupForm = new FormGroup({
+      name: new FormControl('', [Validators.required, Validators.minLength(3)]),
       email: new FormControl('', [Validators.required, Validators.email]),
-      password: new FormControl('', [Validators.required, Validators.minLength(6)])
+      password: new FormControl('', [Validators.required, Validators.minLength(6)]),
+      confirmPassword: new FormControl('', [Validators.required, Validators.minLength(6)])
     });
   }
   
   submit() {
-    this.loginService.login(this.loginForm.value.email, this.loginForm.value.password).subscribe({
+    this.loginService.login(this.signupForm.value.email, this.signupForm.value.password).subscribe({
       next: () => this.toastr.success("Login successful"),
       error: () => this.toastr.error("Error during login")
     })
   }
 
   navigate() {
-    this.router.navigate(["signup"]);
+    this.router.navigate(["login"]);
   
   }
 }
