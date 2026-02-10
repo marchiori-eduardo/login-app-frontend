cc22e39058db0f1e166587e21d1a85fe04444e10
Eduardo Marchiori <marchiorimarchiori2@gmail.com>
2026-02-10 12:06:21 -0300

    feat: finished login page

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -2,10 +2,14 @@ import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
+import { provideHttpClient, withFetch } from '@angular/common/http';
+import { provideToastr } from 'ngx-toastr';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
-    provideRouter(routes)
+    provideRouter(routes),
+    provideToastr(),
+    provideHttpClient(withFetch())
   ]
 };
--- src/app/pages/login/login.ts
+++ src/app/pages/login/login.ts
@@ -3,6 +3,8 @@ import { DefaultLoginLayout } from '../../components/default-login-layout/defaul
 import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 import { PrimaryInput } from '../../components/primary-input/primary-input';
 import { Router } from '@angular/router';
+import { LoginService } from '../../services/login';
+import { ToastrService } from 'ngx-toastr';
 
 @Component({
   selector: 'app-login',
@@ -11,14 +13,19 @@ import { Router } from '@angular/router';
     ReactiveFormsModule,
     PrimaryInput
   ],
+  providers: [
+    LoginService
+  ],
   templateUrl: './login.html',
   styleUrl: './login.scss',
 })
-export class Login {
+export class Login { 
   loginForm!: FormGroup;
 
   constructor(
-    private router: Router
+    private router: Router, 
+    private loginService: LoginService,
+    private toastr: ToastrService
   ) {
     this.loginForm = new FormGroup({
       email: new FormControl('', [Validators.required, Validators.email]),
@@ -27,11 +34,14 @@ export class Login {
   }
   
   submit() {
-    
+    this.loginService.login(this.loginForm.value.email, this.loginForm.value.password).subscribe({
+      next: () => this.toastr.success("Login successful"),
+      error: () => this.toastr.error("Error during login")
+    })
   }
 
   navigate() {
-    this.router.navigate(["/signup"]);
+    this.router.navigate(["signup"]);
   
   }
 }
